37ffe26992ee11516bfc00966c63fa8adb32be0f
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-11 20:52:04 -0300

    urna refactor

--- urnaeletronica/src/main/java/app/urna/UrnaApplication.java
+++ urnaeletronica/src/main/java/app/urna/UrnaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrnaeletronicaApplication {
+public class UrnaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrnaeletronicaApplication.class, args);
+		SpringApplication.run(UrnaApplication.class, args);
 	}
 
 }
