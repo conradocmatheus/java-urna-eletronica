cd70ee679dae2481c737b88307fbe80c55536810
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-11 20:47:34 -0300

    urna firstcommit

---
+++ urnaeletronica/src/main/java/app/urna/UrnaApplication.java
@@ -0,0 +1,13 @@
+package app.urna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrnaeletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrnaeletronicaApplication.class, args);
+	}
+
+}
