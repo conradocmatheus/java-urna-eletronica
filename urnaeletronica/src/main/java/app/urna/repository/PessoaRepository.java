c61de42b9d1efdbfad91af1881c6e26f5ea8b0c4
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-14 20:48:18 -0300

    apaguei pessoa repository

--- urnaeletronica/src/main/java/app/urna/repository/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package app.urna.repository;
-import app.urna.entity.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa,Long>{
-
-}
