c1f58db9fe1984621ee89cd218aeebfb16e533e9
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-14 01:06:37 -0300

    adicionei mais 2 verificacoes no metodo votar

--- urnaeletronica/src/main/java/app/urna/service/VotoService.java
+++ urnaeletronica/src/main/java/app/urna/service/VotoService.java
@@ -1,10 +1,12 @@
 package app.urna.service;
 
 import app.urna.entity.Apuracao;
+import app.urna.entity.Candidato;
 import app.urna.entity.Eleitor;
 import app.urna.entity.Enum.StatusEleitor;
 import app.urna.entity.Voto;
 import app.urna.exception.NotFoundException;
+import app.urna.exception.WrongCandidateException;
 import app.urna.repository.EleitorRepository;
 import app.urna.repository.VotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +36,19 @@ public class VotoService {
         // Verifica se o eleitor esta APTO para votar
         if (!(eleitor.getStatus() == StatusEleitor.APTO)){
             throw new InvalidParameterException("“Eleitor inapto para votacao");
+        }
+
+        Candidato candidatoPrefeito = voto.getPrefeitoEscolhido();
+        Candidato candidatoVereador = voto.getVereadorEscolhido();
+
+        // Verifica se o candidato a prefeito e de fato um candidato a prefeito
+        if (!(candidatoPrefeito.getFuncao() == 1)){
+            throw new WrongCandidateException("O Candidato escolhido nao e um prefeito");
+        }
+
+        // Verifica se o candidato a vereador e de fato um candidato a vereador
+        if (!(candidatoVereador.getFuncao() == 2)){
+            throw new WrongCandidateException("O Candidato escolhido nao e um vereador");
         }
 
         // Seta a hora atual do voto
