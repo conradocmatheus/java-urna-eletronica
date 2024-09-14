912da9cbafd7e9e14b1ffb38deed5e2014e15938
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-14 00:23:45 -0300

    metodos inativar, salvar, atualizar e listar candidatos em CandidatoService

--- urnaeletronica/src/main/java/app/urna/repository/CandidatoRepository.java
+++ urnaeletronica/src/main/java/app/urna/repository/CandidatoRepository.java
@@ -1,11 +1,11 @@
 package app.urna.repository;
 
 import app.urna.entity.Candidato;
+import app.urna.entity.Enum.StatusCandidato;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CandidatoRepository extends JpaRepository <Candidato,Long>{
-    List<Candidato> FindByFuncao(Integer funcao);
-    Candidato FindByNumero(Integer numero);
+    List<Candidato> findAllByStatus(StatusCandidato statusCandidato);
 }
--- urnaeletronica/src/main/java/app/urna/service/CandidatoService.java
+++ urnaeletronica/src/main/java/app/urna/service/CandidatoService.java
@@ -7,19 +7,50 @@ import app.urna.repository.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CandidatoService {
 
     @Autowired
     private CandidatoRepository candidatoRepository;
 
+
+    // Funcao que inativa o candidato
     public void inativarCandidato(Long id) {
 
         // Verifica se o candidato existe por id
         Candidato candidato = candidatoRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Candidato nao encontrado"));
 
-
+        // Inativa e salva o candidato
         candidato.setStatus(StatusCandidato.INATIVO);
+        candidatoRepository.save(candidato);
+    }
+
+    // Salvar candidato
+    public Candidato salvarCandidato(Candidato candidato) {
+        candidato.setStatus(StatusCandidato.ATIVO);
+        return candidatoRepository.save(candidato);
+    }
+
+    // Atualizar candidato
+    public Candidato atualizarCandidato(Long id, Candidato candidatoAtualizado) {
+        // Verifica se o candidato existe por id
+        Candidato candidato = candidatoRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Candidato nao encontrado"));
+
+        // Atualiza os campos do candidato existente
+        candidato.setNomeCompleto(candidatoAtualizado.getNomeCompleto());
+        candidato.setCpf(candidatoAtualizado.getCpf());
+        candidato.setNumeroCandidato(candidatoAtualizado.getNumeroCandidato());
+        candidato.setFuncao(candidatoAtualizado.getFuncao());
+
+        return candidatoRepository.save(candidato);
+    }
+
+    // Lista os candidatos ATIVOS
+    public List<Candidato> listarCandidatos() {
+        return candidatoRepository.findAllByStatus(StatusCandidato.ATIVO);
     }
 }
