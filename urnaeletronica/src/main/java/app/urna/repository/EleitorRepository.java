9565a16c8d43b62f4e1ebe9176a6d294af7a979a
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-13 22:17:09 -0300

    fiz os metodos salvar, atualizar eleitor e um outro metodo privado para validar o status do eleitor

--- urnaeletronica/src/main/java/app/urna/repository/EleitorRepository.java
+++ urnaeletronica/src/main/java/app/urna/repository/EleitorRepository.java
@@ -1,10 +1,11 @@
 package app.urna.repository;
 
 import app.urna.entity.Eleitor;
+import app.urna.entity.Enum.StatusEleitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EleitorRepository extends JpaRepository<Eleitor, Long> {
-    List<Eleitor> FindAllByStatus(String status);
+    List<Eleitor> findAllByStatus(StatusEleitor statusEleitor);
 }
--- urnaeletronica/src/main/java/app/urna/service/EleitorService.java
+++ urnaeletronica/src/main/java/app/urna/service/EleitorService.java
@@ -9,12 +9,13 @@ import app.urna.repository.EleitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EleitorService {
 
     @Autowired
     private EleitorRepository eleitorRepository;
-
 
     // Funcao que inativa o eleitor
     public void inativarEleitor(Long id) {
@@ -24,7 +25,7 @@ public class EleitorService {
                 .orElseThrow(() -> new NotFoundException("Eleitor nao encontrado"));
 
         // Verifica se o eleitor ja votou
-        if (eleitor.getStatus() == StatusEleitor.VOTOU){
+        if (eleitor.getStatus() == StatusEleitor.VOTOU) {
             throw new BussinessException("Eleitor ja votou. Impossivel inativar");
         }
 
@@ -32,4 +33,52 @@ public class EleitorService {
         eleitor.setStatus(StatusEleitor.INATIVO);
         eleitorRepository.save(eleitor);
     }
+
+    public Eleitor salvarEleitor(Eleitor eleitor) {
+        validarStatusEleitor(eleitor);
+        return eleitorRepository.save(eleitor);
+    }
+
+    public Eleitor atualizarEleitor(Long id, Eleitor eleitorAtualizado) {
+        Eleitor eleitor = eleitorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Eleitor não encontrado"));
+
+        // Atualiza os campos do eleitor existente
+        eleitor.setNomeCompleto(eleitorAtualizado.getNomeCompleto());
+        eleitor.setCpf(eleitorAtualizado.getCpf());
+        eleitor.setEmail(eleitorAtualizado.getEmail());
+        eleitor.setTelefoneCelular(eleitorAtualizado.getTelefoneCelular());
+        eleitor.setTelefoneFixo(eleitorAtualizado.getTelefoneFixo());
+        eleitor.setProfissao(eleitorAtualizado.getProfissao());
+
+        validarStatusEleitor(eleitor);
+
+        return eleitorRepository.save(eleitor);
+    }
+
+    // Lista os eleitores com o status APTO
+    public List<Eleitor> listarEleitores() {
+        return eleitorRepository.findAllByStatus(StatusEleitor.APTO);
+    }
+
+    // Esse metodo valida o status do eleitor
+    private void validarStatusEleitor(Eleitor eleitor) {
+        // Variavel que verifica se ha pendencias cadastrais
+        boolean eleitorComPendenciasCadastrais = false;
+
+        // Verifica se o eleitor cadastrou email ou cpf
+        if (eleitor.getCpf().isEmpty() || eleitor.getEmail().isEmpty()) {
+            eleitor.setStatus(StatusEleitor.PENDENTE); // Definir status PENDENTE
+            eleitorComPendenciasCadastrais = true;
+        }
+
+        // Verifica se o eleitor pode ter o status APTO
+        if (!eleitorComPendenciasCadastrais
+                && eleitor.getStatus() != StatusEleitor.INATIVO
+                && eleitor.getStatus() != StatusEleitor.BLOQUEADO
+                && eleitor.getStatus() != StatusEleitor.VOTOU) {
+            eleitor.setStatus(StatusEleitor.APTO); // Eleitor apto
+        }
+    }
+
 }
