ba1d3d54644f6ccf54a54437590eff0a73a6ab33
conradocmatheus <conrado.matheus@protonmail.com>
2024-09-17 09:19:50 -0300

    Apaguei o globalexceptionhandler

--- urnaeletronica/src/main/java/app/urna/handler/exception/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package app.urna.handler.exception;
-
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
-    }
-}
